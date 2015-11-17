99a763d7bafa4872fb10993358b54d05330e4e86
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-11-17 02:40:36 -0600

    works better like this

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+"use strict";
+
 var gulp = require('gulp'),
     β = require('cli-components-build'),
     fs = require('fs'),
     path = require('path')
 
 
-exports.registerTasks = (opts) => {
-    let options = {
-        projectRoot: opts.projectRoot || path.resolve('.'),
-    }
-
-    gulp.task('ts:check-tsconfig', done => {
-        const nonexistent = β.tsconfig().files
-                             .map(file => {
-                                 const fullpath = path.join(options.projectRoot, file)
-                                 return fs.existsSync(fullpath) ? false : fullpath
-                             })
-                             .filter(file => file !== false)
-
-        if (nonexistent.length > 0) {
-            throw new Error('[check-tsconfig] The following files were not found:\n - ' + nonexistent.join('\n - '))
+exports.checkTsconfig = (opts) => {
+    return (done) => {
+        let options = {
+            projectRoot: opts.projectRoot || path.resolve('.'),
         }
-        done()
-    })
+
+        gulp.task('ts:check-tsconfig', done => {
+            const nonexistent = β.tsconfig().files
+                                 .map(file => {
+                                     const fullpath = path.join(options.projectRoot, file)
+                                     return fs.existsSync(fullpath) ? false : fullpath
+                                 })
+                                 .filter(file => file !== false)
+
+            if (nonexistent.length > 0) {
+                throw new Error('[check-tsconfig] The following files were not found:\n - ' + nonexistent.join('\n - '))
+            }
+            done()
+        })
+    }
 }
 
 
