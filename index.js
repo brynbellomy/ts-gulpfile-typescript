bfac2bf369063c8c499b1716c1fd62c43ff50151
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-11-17 23:33:12 -0600

    simplifying

--- index.js
+++ index.js
@@ -7,24 +7,19 @@ var gulp = require('gulp'),
 
 
 exports.checkTsconfig = (opts) => {
-    return (done) => {
-        let options = {
-            projectRoot: opts.projectRoot || path.resolve('.'),
-        }
+    let options = {
+        projectRoot: opts.projectRoot || path.resolve('.'),
+    }
 
-        gulp.task('ts:check-tsconfig', done => {
-            const nonexistent = β.tsconfig().files
-                                 .map(file => {
-                                     const fullpath = path.join(options.projectRoot, file)
-                                     return fs.existsSync(fullpath) ? false : fullpath
-                                 })
-                                 .filter(file => file !== false)
+    const nonexistent = β.tsconfig().files
+                         .map(file => {
+                             const fullpath = path.join(options.projectRoot, file)
+                             return fs.existsSync(fullpath) ? false : fullpath
+                         })
+                         .filter(file => file !== false)
 
-            if (nonexistent.length > 0) {
-                throw new Error('[check-tsconfig] The following files were not found:\n - ' + nonexistent.join('\n - '))
-            }
-            done()
-        })
+    if (nonexistent.length > 0) {
+        throw new Error('[check-tsconfig] The following files were not found:\n - ' + nonexistent.join('\n - '))
     }
 }
 
